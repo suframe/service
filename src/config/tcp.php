7201ac9a73a275d944a04d1b3e9558bfb32c32a2
shulinqian <330576744@qq.com>
2019-07-22 17:00:13 +0800

    修复粘包问题

--- src/config/tcp.php
+++ src/config/tcp.php
@@ -1,10 +1,16 @@
 <?php
 return [
-	'server' => [
-		'listen' => '0.0.0.0',
-		'port' => 8080,
-	],
+    'server' => [
+        'listen' => '0.0.0.0',
+        'port' => 8080,
+    ],
     'swoole' => [
+        'open_length_check' => true,
+        'package_length_type' => 'N',
+        'package_length_offset' => 0,
+        'package_body_offset' => 4,
+        'package_max_length' => 1024 * 1024 * 3, //包大小为3M
+        'buffer_output_size' => 1024 * 1024 * 3,//输出缓存区大小
         'log_file' => SUMMER_APP_ROOT . 'runtime/swoole.log',
         'pid_file' => SUMMER_APP_ROOT . 'runtime/swoole.pid'
     ],
